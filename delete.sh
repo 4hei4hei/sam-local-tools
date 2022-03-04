7422326ee0fec2e68f4c7387d6895da1b465c1b8
4hei4hei <4hei4hei@gmail.com>
2022-03-04 13:56:16 +0900

    fix: delete.sh

--- delete.sh
+++ delete.sh
@@ -5,3 +5,4 @@ set -eo pipefail
 poetry run aws dynamodb delete-table --table-name local-table --endpoint-url http://localhost:8000
 rm -r ./resource/data/*
 docker-compose stop
+docker-compose down
